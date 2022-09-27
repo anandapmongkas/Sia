650d72af363ec25b2601d355dee08534effdf259
anandapmongkas <anandapamongkas@gmail.com>
2022-09-27 07:40:27 +0700

    Data Sekolah

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 8.2.0
+            </div>
+            <strong>Stdanandagzreal &copy; 2014-2023 <a href="https://github.com/">Stdanandagzreal</a>.</strong> All rights
+            reserved.
+        </footer>
